ef936b894369c55ecd049d836dbabd688d94e022
MayadaSamir <mayadasamirm@gmail.com>
2017-02-26 13:40:22 +0200

    Add existing file

---
+++ shop/index.php
@@ -0,0 +1,38 @@
+<?php
+	include("configuration.php");
+	$query="select * from shop";
+	if(isset($_GET['msg'])){
+		echo $_GET['msg'];
+	}
+?>
+<table border="1">
+<tr>
+<td>Id</td>
+<td>Code</td>
+<td>Name</td>
+<td>Description</td>
+<td>Floor_id</td>
+</tr>
+<?php
+	if($resource=mysqli_query($link,$query)){
+		$num=1;
+		while($rowData=mysqli_fetch_assoc($resource)) {
+?>
+<tr>
+<td><?php echo $num; ?></td>
+<td><?php echo $rowData['code'];?></td>
+<td><?php echo $rowData['name'];?></td>
+<td><?php echo $rowData['description'];?></td>
+<td><?php echo $rowData['floor_id'];?></td>
+<td><a href="form.php?cid=<?php echo $rowData['id']; ?>">EDIT</a></td>
+<td><a href="delete.php?cid=<?php echo $rowData['id']; ?>">DELETE</a></td>
+</tr>
+<?php
+		$num ++;
+		}
+	}
+?>
+<tr>
+<td colspan="5"><a href="form.php">Insert New Record</a></td>
+</tr>
+</table>
