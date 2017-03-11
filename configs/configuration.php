61da46a9b038fa99e0025d587a6055918801fb02
Ahmed Elbougha <ahmed.elbougha@gmail.com>
2017-03-11 16:55:47 +0200

    - Restructuring code - start OOP

--- configs/configuration.php
+++ configs/configuration.php
@@ -1,4 +1,14 @@
 <?php
-$site_url = "http://localhost/bleindoornavigation";
-$link = mysqli_connect("localhost","root","root","bleindoornavigation");
+
+function __autoload($classname) {
+    $filename = "../classes/". $classname .".php";
+    include_once($filename);
+}
+
+$host 	  = 'localhost';
+$username = 'root';
+$password = 'root';
+$database = 'bleindoornavigation';
+
+$db = new Db($host, $username, $password, $database);
 ?>
