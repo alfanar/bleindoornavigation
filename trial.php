bb0116d96e35c14ff4d4f348f61bf9fcb47dcbf2
noha <nohanabih22@gmail.com>
2017-04-19 22:22:23 +0200

    Add existing file

---
+++ trial.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<svg width="800" height="700">
+
+
+ <rect x="17" y="0" width="685" height="500" style="fill:white;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />
+ <rect x="17" y="11" width="146" height="113" style="stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9"/>
+  <text x="82" y="62" fill="black">8114</text>
+  
+ <rect x="163" y="0" width="115" height="235" style="fill:blue;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />
+  <text x="221" y="118" fill="black">8115</text>
+ <rect x="17" y="125" width="108" height="105" style="fill:blue;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />
+  <text x="71" y="178" fill="black">8113</text>
+ <rect x="278" y="17" width="97" height="123" style="fill:blue;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />
+  <text x="327" y="79" fill="black">8111</text>
+ <rect x="278" y="140" width="62" height="95" style="fill:blue;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />  
+  <text x="300" y="188" font-size="10" fill="black">8115</text>
+ <rect x="375" y="17" width="60" height="218" style="fill:blue;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />  
+  <text x="395" y="126" font-size="10" fill="black">8115</text>
+ <rect x="435" y="0" width="117" height="235" style="fill:blue;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />  
+  <text x="494" y="118" fill="black">8115</text>
+ <rect x="552" y="21" width="141" height="104" style="fill:blue;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />  
+   <text x="623" y="73"  fill="black">8115</text>
+ <rect x="585" y="128" width="108" height="110" style="fill:blue;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />
+   <text x="639" y="183"  fill="black">8115</text>
+ <rect x="17" y="311" width="255" height="169" style="fill:blue;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />
+   <text x="145" y="396" fill="black">8115</text>
+ <rect x="272" y="311" width="53" height="119" style="fill:blue;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />
+   <text x="290" y="371" font-size="10" fill="black">8115</text>
+ <rect x="390" y="311" width="53" height="119" style="fill:blue;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />
+   <text x="405" y="370" font-size="10" fill="black">8115</text>
+ <rect x="443" y="311" width="255" height="169" style="fill:blue;stroke:black;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />
+   <text x="571" y="396" fill="black">8115</text>
+ 
+</svg>
+
+
+</body>
+</html>
